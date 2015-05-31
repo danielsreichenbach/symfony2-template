0a7ce4ac30224018e73218f60560a5a757fb5bea
Daniel S. Reichenbach <daniel@kogitoapp.com>
2015-05-31 22:18:38 +0200

    Added user event listener for common tasks
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

---
+++ src/AppBundle/EventListener/UserEventListener.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+
+/**
+ * React to FOSUserBundle events and hook into interesting events
+ *
+ * @author Daniel S. Reichenbach <daniel@kogitoapp.com>
+ */
+class UserEventListener implements EventSubscriberInterface
+{
+    /**
+     * @var EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * Injecting the requirements
+     *
+     * @param EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FOSUserEvents::REGISTRATION_SUCCESS => 'onRegistrationSuccess',
+            FOSUserEvents::REGISTRATION_CONFIRMED => 'onRegistrationConfirmed',
+            FOSUserEvents::CHANGE_PASSWORD_SUCCESS => 'onChangePasswordSuccess',
+            FOSUserEvents::RESETTING_RESET_SUCCESS => 'onResettingResetSuccess',
+        );
+    }
+
+    /**
+     * React to a successful registration prior to encrypting the password
+     *
+     * @param FilterUserResponseEvent $event
+     */
+    public function onRegistrationSuccess(FormEvent $event)
+    {
+        /** @var \AppBundle\Entity\User $user */
+        $user = $event->getForm()->getData();
+
+        // ... do something which requires access to the unencrypted password
+    }
+
+    /**
+     * React to a confirmed registration
+     *
+     * @param FilterUserResponseEvent $event
+     */
+    public function onRegistrationConfirmed(FilterUserResponseEvent $event)
+    {
+        /** @var \AppBundle\Entity\User $user */
+        $user = $event->getUser();
+
+        // ... do something after a user is confirmed, e.g send a welcome message
+    }
+
+    /**
+     * React to a password update prior to encrypting the password
+     *
+     * @param FilterUserResponseEvent $event
+     */
+    public function onChangePasswordSuccess(FormEvent $event)
+    {
+        /** @var \AppBundle\Entity\User $user */
+        $user = $event->getForm()->getData();
+
+        // ... do something which requires access to the unencrypted password
+    }
+
+    /**
+     * React to a password reset prior to encrypting the password
+     *
+     * @param FilterUserResponseEvent $event
+     */
+    public function onResettingResetSuccess(FormEvent $event)
+    {
+        /** @var \AppBundle\Entity\User $user */
+        $user = $event->getForm()->getData();
+
+        // ... do something which requires access to the unencrypted password
+    }
+}
