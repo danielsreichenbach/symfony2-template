d6371e76f7ff527b0fccfa0e1c0b3df080e6cce7
Daniel S. Reichenbach <daniel@kogitoapp.com>
2015-06-01 18:04:16 +0200

    Updated migration
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- app/migrations/Version20150207115633.php
+++ app/migrations/Version20150207115633.php
@@ -10,7 +10,7 @@ use Doctrine\DBAL\Schema\Schema;
  *
  * @author  Daniel S. Reichenbach <daniel@kogitoapp.com>
  */
-class Version20150531170948 extends AbstractMigration
+class Version20150207115633 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -22,9 +22,9 @@ class Version20150531170948 extends AbstractMigration
 
         $this->addSql('CREATE TABLE fos_groups (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', UNIQUE INDEX UNIQ_A44C5CA5E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE fos_users (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, username_canonical VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, email_canonical VARCHAR(255) NOT NULL, enabled TINYINT(1) NOT NULL, salt VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, last_login DATETIME DEFAULT NULL, locked TINYINT(1) NOT NULL, expired TINYINT(1) NOT NULL, expires_at DATETIME DEFAULT NULL, confirmation_token VARCHAR(255) DEFAULT NULL, password_requested_at DATETIME DEFAULT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', credentials_expired TINYINT(1) NOT NULL, credentials_expire_at DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_32427CF792FC23A8 (username_canonical), UNIQUE INDEX UNIQ_32427CF7A0D96FBF (email_canonical), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE fos_users_groups (user_id INT NOT NULL, group_id INT NOT NULL, INDEX IDX_B3C77447A76ED395 (user_id), INDEX IDX_B3C77447FE54D947 (group_id), PRIMARY KEY(user_id, group_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE fos_users_groups ADD CONSTRAINT FK_B3C77447A76ED395 FOREIGN KEY (user_id) REFERENCES fos_users (id)');
-        $this->addSql('ALTER TABLE fos_users_groups ADD CONSTRAINT FK_B3C77447FE54D947 FOREIGN KEY (group_id) REFERENCES fos_groups (id)');
+        $this->addSql('CREATE TABLE fos_users_groups (user_id INT NOT NULL, group_id INT NOT NULL, INDEX IDX_AC762C1DA76ED395 (user_id), INDEX IDX_AC762C1DFE54D947 (group_id), PRIMARY KEY(user_id, group_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE fos_users_groups ADD CONSTRAINT FK_AC762C1DA76ED395 FOREIGN KEY (user_id) REFERENCES fos_users (id)');
+        $this->addSql('ALTER TABLE fos_users_groups ADD CONSTRAINT FK_AC762C1DFE54D947 FOREIGN KEY (group_id) REFERENCES fos_groups (id)');
     }
 
     /**
@@ -35,8 +35,8 @@ class Version20150531170948 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE fos_users_groups DROP FOREIGN KEY FK_B3C77447FE54D947');
-        $this->addSql('ALTER TABLE fos_users_groups DROP FOREIGN KEY FK_B3C77447A76ED395');
+        $this->addSql('ALTER TABLE fos_users_groups DROP FOREIGN KEY FK_AC762C1DFE54D947');
+        $this->addSql('ALTER TABLE fos_users_groups DROP FOREIGN KEY FK_AC762C1DA76ED395');
         $this->addSql('DROP TABLE fos_groups');
         $this->addSql('DROP TABLE fos_users');
         $this->addSql('DROP TABLE fos_users_groups');
