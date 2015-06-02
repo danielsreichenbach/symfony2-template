8b966bd684e1a60a4e1fa5daeb82ee006b440353
Daniel S. Reichenbach <daniel@kogitoapp.com>
2015-06-02 10:09:54 +0200

    Added fixture for unconfirmed user
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- src/AppBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -38,11 +38,16 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, C
          */
         $userManager = $this->container->get('fos_user.user_manager');
 
+        /**
+         * @var \FOS\UserBundle\Util\TokenGeneratorInterface
+         */
+        $tokenGenerator = $this->container->get('fos_user.util.token_generator');
+
         /** @var \AppBundle\Entity\User $administrator */
         $administrator = $userManager->createUser();
         $administrator->setUsername('administrator');
         $administrator->setEmail('admin@symfony2-template.io');
-        $administrator->setPlainPassword('administrator');
+        $administrator->setPlainPassword('test');
         $administrator->setEnabled(true);
         $administrator->setSuperAdmin(true);
         $administrator->setLocale('en');
@@ -54,12 +59,24 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, C
         $defaultUser = $userManager->createUser();
         $defaultUser->setUsername('user');
         $defaultUser->setEmail('user@symfony2-template.io');
-        $defaultUser->setPlainPassword('user');
+        $defaultUser->setPlainPassword('test');
         $defaultUser->setEnabled(true);
         $defaultUser->setLocale('de');
 
         $userManager->updateUser($defaultUser, true);
         $this->addReference('user-default', $defaultUser);
+
+        /** @var \AppBundle\Entity\User $unconfirmedUser */
+        $unconfirmedUser = $userManager->createUser();
+        $unconfirmedUser->setUsername('user-unconfirmed');
+        $unconfirmedUser->setEmail('user-unconfirmed@symfony2-template.io');
+        $unconfirmedUser->setPlainPassword('test');
+        $unconfirmedUser->setEnabled(false);
+        $unconfirmedUser->setLocale('en');
+        $unconfirmedUser->setConfirmationToken($tokenGenerator->generateToken());
+
+        $userManager->updateUser($unconfirmedUser, true);
+        $this->addReference('user-unconfirmed', $unconfirmedUser);
 
         $manager->flush();
     }
