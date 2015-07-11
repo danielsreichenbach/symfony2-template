aec719d6a1d8b6b5c78ab4b16b3ac8b4707812ad
Daniel S. Reichenbach <daniel@kogitoapp.com>
2015-07-11 10:32:59 +0200

    Switch to XML routing configuration
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -14,7 +13,6 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
      * @return Response
      */
     public function indexAction()
